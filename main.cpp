321e8de2a966e9e55db091fbfce0ad48a433791a
Momen Ibrahim Fawzy <momenibrahimfawzy982000@gmail.com>
2022-12-03 14:29:17 +0200

    Compile The Whole Project
    add to the main the whole project

--- main.cpp
+++ main.cpp
@@ -13,7 +13,30 @@ int main() {
     NFA nfa("grammar_input.txt");
     DFA dfa(nfa);
     dfa.run();
-    dfa.get_DFA();
-    int x = 0;
+    TransitionTable TT = dfa.get_DFA();
+    ///get the lexical analyzer
+    LexicalAnalyzer LA(TT);
+
+    string input_text="";
+    ifstream myfile; myfile.open("C:\\Users\\momen\\OneDrive\\Documents\\GitHub\\Java-Compiler\\test_program.txt");
+    if ( myfile.is_open() ) {
+        char mychar;
+        while ( myfile ) {
+            mychar = myfile.get();
+            input_text+=mychar;
+        }
+    }
+    cout << input_text << "\n";
+
+    //split the input text on the white space
+    vector<string> words = split_string_by_white_spaces(input_text);
+    for (auto word = words.begin(); word != words.end(); ++word) {
+//        cout << *word << "\n";
+        vector<pair<string, string>> tokens = LA.getAllTokensInText(*word);
+        for (auto i = tokens.begin(); i != tokens.end(); ++i) {
+            pair<string, string> token = *i;
+            cout << token.first << " : " << token.second << "\n";
+        }
+    }
     return 0;
 }
